178241a522f7ab92f4f32de65e5babe657ebd4af
anas-shakeel <srdranas456@gmail.com>
2024-11-26 00:27:48 +0500

    Add argument validations

--- maketree/cli.py
+++ maketree/cli.py
@@ -3,6 +3,7 @@
 import sys
 from argparse import ArgumentParser
 from maketree.core.parser import Parser
+from pathlib import Path
 
 
 PROGRAM = "maketree"
@@ -12,7 +13,28 @@ VERSION = "0.0.1"
 def main():
     args = parse_args()
 
-    print(args)
+    sourcefile = Path(args.src)
+    dstpath = Path(args.dst)
+
+    # SRC Exists?
+    if not sourcefile.exists():
+        print("source '%s' does not exist." % sourcefile)
+        sys.exit(1)
+
+    # SRC Tree file?
+    if not sourcefile.name.endswith(".tree"):
+        print("source '%s' is not a .tree file." % sourcefile)
+        sys.exit(1)
+
+    # DST Exists?
+    if not dstpath.exists():
+        print("destination path '%s' does not exist." % dstpath)
+        sys.exit(1)
+
+    # DST not a Dir?
+    if not dstpath.is_dir():
+        print("destination path '%s' is not a directory." % dstpath)
+        sys.exit(1)
 
 
 def parse_args():
