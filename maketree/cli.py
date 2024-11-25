4af8c6b0bfaeffb53bd087f8f5ebcbc1825983b0
anas-shakeel <srdranas456@gmail.com>
2024-11-26 00:07:38 +0500

    Add parse_args function

--- maketree/cli.py
+++ maketree/cli.py
@@ -1,8 +1,38 @@
 """ Frontend of the project (Argument handling and stuff) """
 
+import sys
+from argparse import ArgumentParser
+from maketree.core.parser import Parser
+
+
 PROGRAM = "maketree"
 VERSION = "0.0.1"
 
 
 def main():
-    print(PROGRAM, VERSION)
+    args = parse_args()
+
+    print(args)
+
+
+def parse_args():
+    """Parse command-line arguments and return."""
+
+    parser = ArgumentParser(
+        prog=PROGRAM,
+        usage="%(prog)s [OPTIONS]",
+        description="A CLI tool to create directory structures from a structure file.",
+    )
+
+    parser.add_argument(
+        "-v", "--version", action="version", version="%s %s" % (PROGRAM, VERSION)
+    )
+    parser.add_argument("src", help="source file (with .tree extension)")
+    parser.add_argument(
+        "dst",
+        nargs="?",
+        default=".",
+        help="where to create the tree structure (default: %(default)s)",
+    )
+
+    return parser.parse_args()
