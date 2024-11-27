9c69adddf7e17e53953c0ce14ec14f535a732c9c
anas-shakeel <srdranas456@gmail.com>
2024-11-28 01:09:12 +0500

    Replace lists with sets

--- maketree/core/normalizer.py
+++ maketree/core/normalizer.py
@@ -1,16 +1,32 @@
 """ Normalizes the parsed tree and creates paths. """
 
 from os.path import join as join_path
-from typing import List, Dict
+from typing import List, Dict, Set
 
 
 class Normalizer:
 
     @classmethod
-    def normalize(cls, tree: List[Dict]) -> Dict[str, List[str]]:
-        """Normalizes tree as paths. Returns a dictionary of lists of file and dir paths."""
-        dirs = []  # Holds normalized dirs
-        files = []  # Holds normalized files
+    def normalize(cls, tree: List[Dict]) -> Dict[str, Set[str]]:
+        """
+        Normalizes tree as paths and remove any duplicate paths.
+        Returns a dictionary of Two Sets containing file and dir paths.
+
+        ```
+        # Output Dict
+        {
+            "directories": {'./src', './docs'}
+            "files": {
+                'index.html',
+                'styles.css',
+                'user-guide.md',
+                'dev-guide.md'
+            }
+        }
+        ```
+        """
+        dirs = set()  # Holds normalized dirs
+        files = set()  # Holds normalized files
 
         def traverse(node: Dict, path: List):
             for child in node.get("children", []):
@@ -18,12 +34,12 @@ class Normalizer:
                 str_path = join_path(*path, name)
 
                 if child["type"] == "directory":
-                    dirs.append(str_path)
+                    dirs.add(str_path)
                     # Got Children?
                     if child["children"]:
                         traverse(child, path + [name])
                 else:  # File
-                    files.append(str_path)
+                    files.add(str_path)
 
         traverse(
             node={
