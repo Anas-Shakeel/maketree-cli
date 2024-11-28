7c494546765b6ee5e6bf4d59f433baaaf2257e18
anas-shakeel <srdranas456@gmail.com>
2024-11-28 22:36:38 +0500

    Return number of files/dirs created

--- maketree/core/tree_builder.py
+++ maketree/core/tree_builder.py
@@ -3,14 +3,14 @@ based on the parsed data from the structure file. """
 
 import os
 from os.path import exists
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 
 class TreeBuilder:
     """Build the tree parsed from `.tree` file"""
 
     @classmethod
-    def build(cls, paths: Dict[str, List[str]], skip: bool = False):
+    def build(cls, paths: Dict[str, List[str]], skip: bool = False) -> Tuple[int, int]:
         """
         ### Build
         Create the directories and files on the filesystem.
@@ -18,23 +18,32 @@ class TreeBuilder:
         #### Args:
         - `paths`: the paths dictionary
         - `skip`: skips existing files
+
+        Returns a `tuple[int, int]` containing the number of
+        dirs and files created, in that order.
         """
-        cls.create_dirs(paths["directories"])
-        cls.create_files(paths["files"], skip=skip)
+        dirs_created = cls.create_dirs(paths["directories"])
+        files_created = cls.create_files(paths["files"], skip=skip)
+
+        return (dirs_created, files_created)
 
     @classmethod
-    def create_dirs(cls, dirs: List[str]):
-        """Create files with names found in `files`."""
+    def create_dirs(cls, dirs: List[str]) -> int:
+        """Create files with names found in `files`. Returns the number of dirs created."""
+        count = 0
         for path in dirs:
             try:
                 # Create the directory
                 os.mkdir(path)
+                count += 1
             except FileExistsError:
                 pass
+        return count
 
     @classmethod
-    def create_files(cls, files: List[str], skip: bool = False):
-        """Create files with names found in `files`."""
+    def create_files(cls, files: List[str], skip: bool = False) -> int:
+        """Create files with names found in `files`. Returns the number of files created."""
+        count = 0
         for path in files:
             if skip and exists(path):
                 continue
@@ -42,3 +51,6 @@ class TreeBuilder:
             # Create the file
             with open(path, "w") as _:
                 pass  # Empty file
+            count += 1
+
+        return count
