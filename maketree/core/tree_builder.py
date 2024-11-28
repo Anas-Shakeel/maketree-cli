81a87c379b40896d5e45344b23af5fc0c9523f70
anas-shakeel <srdranas456@gmail.com>
2024-11-28 19:19:46 +0500

    Integerate skip option

--- maketree/core/tree_builder.py
+++ maketree/core/tree_builder.py
@@ -2,6 +2,7 @@
 based on the parsed data from the structure file. """
 
 import os
+from os.path import exists
 from typing import List, Dict
 
 
@@ -9,22 +10,35 @@ class TreeBuilder:
     """Build the tree parsed from `.tree` file"""
 
     @classmethod
-    def build(cls, paths: Dict[str, List[str]]):
-        """Create the directories and files on the filesystem."""
+    def build(cls, paths: Dict[str, List[str]], skip: bool = False):
+        """
+        ### Build
+        Create the directories and files on the filesystem.
+
+        #### Args:
+        - `paths`: the paths dictionary
+        - `skip`: skips existing files
+        """
         cls.create_dirs(paths["directories"])
-        cls.create_files(paths["files"])
+        cls.create_files(paths["files"], skip=skip)
 
     @classmethod
     def create_dirs(cls, dirs: List[str]):
         """Create files with names found in `files`."""
         for path in dirs:
-            # Create the directory
-            os.mkdir(path)
+            try:
+                # Create the directory
+                os.mkdir(path)
+            except FileExistsError:
+                pass
 
     @classmethod
-    def create_files(cls, files: List[str]):
+    def create_files(cls, files: List[str], skip: bool = False):
         """Create files with names found in `files`."""
         for path in files:
+            if skip and exists(path):
+                continue
+
             # Create the file
-            with open(path, "x") as _:
+            with open(path, "w") as _:
                 pass  # Empty file
