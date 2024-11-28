3e4a40f215ee865c8555d452a21d8ed687e8d434
anas-shakeel <srdranas456@gmail.com>
2024-11-28 18:44:19 +0500

    Add remove_existing_paths function

--- maketree/utils.py
+++ maketree/utils.py
@@ -1,1 +1,9 @@
 """ Contains Helper code to keep core logic clean. (things that don't fit anywhere, fit here) """
+
+from os.path import exists
+from typing import List
+
+
+def remove_existing_paths(paths: List[str]) -> List[str]:
+    """Remove existing paths from `paths` list and return the list."""
+    return list(filter(lambda p: not exists(p), paths))
