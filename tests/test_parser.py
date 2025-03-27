ad888189d70ef87ba8eefc1de3693e0a9f1c0a3e
anas-shakeel <srdranas456@gmail.com>
2025-03-28 02:17:46 +0500

    Fix test_parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -13,16 +13,22 @@ README.md
 """
     expected_tree = [
         {
-            "type": "directory",
             "name": "src",
+            "type": "directory",
+            "line": 1,
+            "indent": 0,
             "children": [
-                {"type": "file", "name": "file1.txt"},
-                {"type": "file", "name": "file2.txt"},
+                {"name": "file1.txt", "type": "file", "line": 2, "indent": 1},
+                {"name": "file2.txt", "type": "file", "line": 3, "indent": 1},
             ],
         },
-        {"type": "file", "name": "LICENSE"},
-        {"type": "file", "name": "README.md"},
+        {"name": "LICENSE", "type": "file", "line": 4, "indent": 0},
+        {"name": "README.md", "type": "file", "line": 5, "indent": 0},
     ]
 
-    assert Parser._parse_lines(structure.splitlines()) == expected_tree
+    parsed_tree = Parser._parse_lines(structure.splitlines())
+
+    assert len(parsed_tree) == len(expected_tree)
+    assert parsed_tree[0]["name"] == expected_tree[0]["name"]
+    assert parsed_tree[1]["type"] == expected_tree[1]["type"]
     assert Parser._parse_lines(["", "", ""]) == []  # Empty lines
