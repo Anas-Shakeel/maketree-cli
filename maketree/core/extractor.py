98ca6b9747c90c77078bd9516d77e84a0d35a459
anas-shakeel <srdranas456@gmail.com>
2025-04-01 09:45:24 +0500

    Return the extracted tree instead of writing to file

--- maketree/core/extractor.py
+++ maketree/core/extractor.py
@@ -1,9 +1,10 @@
 """Extracts a directory tree and writes into a file. (preferably .tree)"""
 
 from pathlib import Path
-from typing import Optional
 from maketree.console import Console
 from maketree.utils import incremented_filename, now
+
+from typing import Optional, List, Tuple
 
 
 class Extractor:
@@ -14,38 +15,37 @@ class Extractor:
         cls,
         path: Path,
         console: Optional[Console] = None,
-    ) -> str:
+    ) -> List[Tuple[str, str, int]]:
         """
         ### Extract
-        Extract the directory structure.
+        Extract the directory structure and return the extracted tree list.
 
         #### Args:
         - `path`: path to a directory (must be a `Path` object)
 
-        Returns the name of the file created. (filename taken from the root directory's name)
+        #### Output Tree Structure:
+        ```
+        tree = [
+            (TYPE, NAME, DEPTH),
+        ]
+        ```
         """
         path = path.absolute()  # Path to absolute
-        spacer = "    "  # Spacer for indentation
-
-        # Non-Existent filename
-        filename = path.name or now("%d%m%Y-%H%M%S")  # Folder-Name or Timestamp
-        filename = incremented_filename("%s.tree" % filename)
-
-        console.verbose("Creating %s..." % filename)
+        tree: List[Tuple[str, str, int]] = []
 
         # Write stuff to stuff.stuff :)
-        with open(filename, "w", encoding="utf-8") as f:
-            for current, _, files in path.walk():
-                depth = len(current.relative_to(path).parts)  # Indent depth
-                dir_name = current.name or str(current)  # Current dir name
+        for current, _, files in path.walk():
+            depth = len(current.relative_to(path).parts)
+            dir_name = current.name or str(current)
 
-                console.verbose("found %s/..." % dir_name)
-                # Write directory line
-                f.write("%s%s/\n" % (spacer * depth, dir_name))
+            console.verbose("found %s/..." % dir_name)
 
-                # Write file lines
-                for file in files:
-                    console.verbose("found %s..." % file)
-                    f.write("%s%s\n" % (spacer * (depth + 1), file))
+            # Append directory line
+            tree.append(("directory", dir_name, depth))
 
-        return filename
+            # Append file lines
+            for file in files:
+                console.verbose("found %s..." % file)
+                tree.append(("file", file, (depth + 1)))
+
+        return tree
